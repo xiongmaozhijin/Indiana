563f456281eda085bbd27f8c22b2713a0665d356
baoxing <312904694@qq.com>
2015-12-21 19:58:26 +0800

    完成商品详情界面

---
+++ app/src/main/java/com/example/liangge/indiana/ui/ProductDetailInfoActivity.java
@@ -0,0 +1,24 @@
+package com.example.liangge.indiana.ui;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.liangge.indiana.R;
+
+/**
+ * 商品详情
+ */
+public class ProductDetailInfoActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail_info);
+
+
+    }
+
+
+}
